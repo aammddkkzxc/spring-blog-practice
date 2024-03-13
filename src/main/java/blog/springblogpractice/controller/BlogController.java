dcd946fc88d5f4f92f1e58c97935b16d22e464ef
aammddkkzxc <ideal0427@naver.com>
2024-03-13 21:34:40 +0900

    feat: 글 목록 조회 매핑 구현

--- src/main/java/blog/springblogpractice/controller/BlogController.java
+++ src/main/java/blog/springblogpractice/controller/BlogController.java
@@ -2,10 +2,13 @@ package blog.springblogpractice.controller;
 
 import blog.springblogpractice.domain.Article;
 import blog.springblogpractice.dto.AddArticleRequest;
+import blog.springblogpractice.dto.ArticleResponse;
 import blog.springblogpractice.service.BlogService;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,5 +26,13 @@ public class BlogController {
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest addArticleRequest) {
         Article savedArticle = blogService.save(addArticleRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    @ResponseBody
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll().stream()
+                .map(ArticleResponse::new).toList();
+        return ResponseEntity.status(HttpStatus.OK).body(articles);
     }
 }
