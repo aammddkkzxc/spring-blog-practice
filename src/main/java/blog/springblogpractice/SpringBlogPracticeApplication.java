6d68206eff6f6400f70a6fcbdf86b21f2288618a
aammddkkzxc <ideal0427@naver.com>
2024-03-13 11:46:00 +0900

    feat: 세팅

---
+++ src/main/java/blog/springblogpractice/SpringBlogPracticeApplication.java
@@ -0,0 +1,13 @@
+package blog.springblogpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogPracticeApplication.class, args);
+	}
+
+}
