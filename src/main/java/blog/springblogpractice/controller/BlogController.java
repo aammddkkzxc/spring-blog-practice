76f87552de28875fb4891d9f64e88c91225033c4
aammddkkzxc <ideal0427@naver.com>
2024-03-13 22:33:47 +0900

    feat: 삭제 요청 매핑

--- src/main/java/blog/springblogpractice/controller/BlogController.java
+++ src/main/java/blog/springblogpractice/controller/BlogController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,5 +44,11 @@ public class BlogController {
         Article article = blogService.findById(id);
 
         return ResponseEntity.status(HttpStatus.FOUND).body(article);
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    @ResponseBody
+    public ResponseEntity<Void> deleteById(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
