3de2a94f18cd030c54ae30d88bfdc88601815c83
aammddkkzxc <ideal0427@naver.com>
2024-03-13 22:18:19 +0900

    feat: 글 단건 조회 매핑 구현

--- src/main/java/blog/springblogpractice/controller/BlogController.java
+++ src/main/java/blog/springblogpractice/controller/BlogController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -34,5 +35,13 @@ public class BlogController {
         List<ArticleResponse> articles = blogService.findAll().stream()
                 .map(ArticleResponse::new).toList();
         return ResponseEntity.status(HttpStatus.OK).body(articles);
+    }
+
+    @GetMapping("/api/articles/{id}")
+    @ResponseBody
+    public ResponseEntity<Article> findById(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.status(HttpStatus.FOUND).body(article);
     }
 }
