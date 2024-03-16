42a3e79d7f150a23d9008330e22a0f453ab8ac31
aammddkkzxc <ideal0427@naver.com>
2024-03-16 16:15:48 +0900

    feat: 글 수정/생성 매핑

--- src/main/java/blog/springblogpractice/controller/BlogViewController.java
+++ src/main/java/blog/springblogpractice/controller/BlogViewController.java
@@ -1,12 +1,15 @@
 package blog.springblogpractice.controller;
 
+import blog.springblogpractice.domain.Article;
 import blog.springblogpractice.dto.ArticleListViewResponse;
+import blog.springblogpractice.dto.ArticleViewResponse;
 import blog.springblogpractice.service.BlogService;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
@@ -20,5 +23,16 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
     }
 }
