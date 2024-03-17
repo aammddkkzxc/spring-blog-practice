4fcb13cd61570404cccc9b77b08dbf0185aca8ae
aammddkkzxc <ideal0427@naver.com>
2024-03-17 16:28:19 +0900

    feat: comment 초기 데이터

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목1', '내용1', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목2', '내용2', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목3', '내용3', NOW(), NOW());
+
+INSERT INTO comment (body, created_at) VALUES ('댓글 내용1', NOW());
+INSERT INTO comment (body, created_at) VALUES ('댓글 내용2', NOW());
+INSERT INTO comment (body, created_at) VALUES ('댓글 내용3', NOW());
